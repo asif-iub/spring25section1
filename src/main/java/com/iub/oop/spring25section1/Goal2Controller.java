8b670c9b6844cb913b0e47bfb2eac014b3357bd9
Asif Mahmood <asif.mahmood@iub.edu.bd>
2025-04-13 09:25:16 +0600

    Added text file read and write

--- src/main/java/com/iub/oop/spring25section1/Goal2Controller.java
+++ src/main/java/com/iub/oop/spring25section1/Goal2Controller.java
@@ -1,27 +1,33 @@
 package com.iub.oop.spring25section1;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.*;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Goal2Controller extends BaseController
 {
     @javafx.fxml.FXML
+    private TextArea textArea;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private CheckBox appendCheckBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }
 
-//    @javafx.fxml.FXML
-//    public void goBack(ActionEvent actionEvent) throws IOException {
-//        SceneSwitcher.switchTo("dashboard");
-//    }
-//
-//    @javafx.fxml.FXML
-//    public void logOut(ActionEvent actionEvent) throws IOException{
-//        SceneSwitcher.switchTo("login");
-//    }
+    @javafx.fxml.FXML
+    public void writeToFile(ActionEvent actionEvent){
+//        FileWriter fileWriter = null;
+        try (FileWriter fileWriter = new FileWriter("data.txt")){
+            fileWriter.write(textArea.getText());
+            label.setText("Saved to 'data.txt'");
+        } catch(IOException e){
+            label.setText("Could not write to file!");
+        }
+    }
 }
--- src/main/java/com/iub/oop/spring25section1/Goal3Controller.java
+++ src/main/java/com/iub/oop/spring25section1/Goal3Controller.java
@@ -1,27 +1,37 @@
 package com.iub.oop.spring25section1;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
+import java.io.FileReader;
 import java.io.IOException;
 
-public class Goal3Controller extends BaseController
-{
+public class Goal3Controller extends BaseController {
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TextArea textArea;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
 
-//    @javafx.fxml.FXML
-//    public void goBack(ActionEvent actionEvent) throws IOException {
-//        SceneSwitcher.switchTo("dashboard");
-//    }
-//
-//    @javafx.fxml.FXML
-//    public void logOut(ActionEvent actionEvent) throws IOException{
-//        SceneSwitcher.switchTo("login");
-//    }
+    @javafx.fxml.FXML
+    public void loadFromFile(ActionEvent actionEvent) {
+        try (FileReader reader = new FileReader("data.txt");) {
+            String content = "";
+            while(true) {
+                int i = reader.read();
+                if (i == -1)
+                    break;
+
+                content = content + (char) i;
+            }
+            textArea.setText(content);
+        }
+        catch (IOException e) {
+            label.setText("Could not load from file!");
+        }
+    }
 }
